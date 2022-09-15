9d9d6a2874a5e46e36667c5378799132931d5c9d
Gabriel Cesario <gcesario203@gmail.com>
2022-09-14 23:06:43 -0300

    feat: Testes de leitura e criação de venda

--- unitTests/OrderUnitTests.cs
+++ unitTests/OrderUnitTests.cs
@@ -1,5 +1,5 @@
-using implementation.Data;
 using implementation.Models;
+using implementation.Models.Enums;
 using unitTests.Utils.MockBuilders;
 using static implementation.Utils.Helpers.ControllerHelpers;
 using static unitTests.Utils.Helpers.OrderHelpers;
@@ -8,6 +8,27 @@ namespace unitTests
 {
     public class OrderUnitTests
     {
+
+        [Fact]
+        public void ShoudlGetTheFourthOrder()
+        {
+            var mockBuilder = new OrderMockBuilder();
+
+            var result = mockBuilder.OrderController.GetById(4);
+
+            Assert.NotEqual(HandleActionResult<Order>(result), null);
+        }
+
+        [Fact]
+        public void ShouldNotFindThirtyFifthOrder()
+        {
+            var mockBuilder = new OrderMockBuilder();
+
+            var result = mockBuilder.OrderController.GetById(35);
+
+            Assert.Equal(HandleActionResult<Order>(result), null);
+        }
+
         [Fact]
         public void ShouldCreateTheFifthUserOfTheSystem()
         {
@@ -19,8 +40,26 @@ namespace unitTests
         }
 
         [Fact]
-        public void ShouldTheNewUser()
+        public void Should()
         {
+            var mockBuilder = new OrderMockBuilder();
+
+            var result = mockBuilder.OrderController.Create(MockUser());
+
+            Assert.Equal(HandleActionResult<Order>(result).Id, 5);
+        }
+
+        [Fact]
+        public void ShouldTheNewUserStartWithPendingPaymentStatus()
+        {
+            var mockBuilder = new OrderMockBuilder();
+            var mockUser = MockUser();
+
+            mockUser.Status = OrderStatus.ApprovedPayment;
+
+            var result = mockBuilder.OrderController.Create(mockUser);
+
+            Assert.Equal(HandleActionResult<Order>(result).Status, OrderStatus.WaitingPayment);
         }
     }
 }
