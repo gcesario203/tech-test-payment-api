f39cba7742196a18581d4988d96ecbe69ba18ff5
Gabriel Cesario <gcesario203@gmail.com>
2022-09-15 01:13:46 -0300

    fix: esqueci de mudar a URL para documentação do Swagger

--- implementation/Program.cs
+++ implementation/Program.cs
@@ -33,7 +33,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = "api-docs";
+    });
 }
 
 // app.UseHttpsRedirection();
